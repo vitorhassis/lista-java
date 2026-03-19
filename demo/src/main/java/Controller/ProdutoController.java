a182cdc48dab1c2b99caf2168ecc1e1e084d4eab
Vitor Hugo Assis <vitorassis997@gmail.com>
2026-03-19 15:45:01 -0300

    adiciona produto controller

---
+++ demo/src/main/java/Controller/ProdutoController.java
@@ -0,0 +1,58 @@
+package Controller;
+
+import Entity.Produto;
+import Service.ProdutoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping
+public class ProdutoController {
+    private final ProdutoService service;
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criar(Produto produto){
+        Produto request = service.adicionar(produto);
+        URI uri;
+        uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(request.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> listar(){
+        List<Produto> produtos = service.listar();
+        return  ResponseEntity.ok().body(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable long id){
+        var produto = service.buscarPorId(id);
+
+        if(produto.isEmpty()) {
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(produto.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable long id) {
+        Optional<Produto> produto = service.buscarPorId(id);
+
+        if(produto.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
