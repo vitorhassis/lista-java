49e89ff100d73f0285214ec2967b83c669a0b235
Vitor Hugo Assis <vitorassis997@gmail.com>
2026-03-19 15:39:23 -0300

    adiciona controller

---
+++ demo/src/main/java/Controller/LivroController.java
@@ -0,0 +1,61 @@
+package Controller;
+
+import Entity.Livro;
+import Service.LivroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/livros")
+public class LivroController {
+    private final LivroService service;
+
+    public LivroController(LivroService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> salvar(@RequestBody Livro livro){
+        Livro request =  service.adicionar(livro);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(request.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Livro>> listar(){
+        List<Livro> livros = service.listar();
+        return ResponseEntity.ok(livros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> buscarPorId(@PathVariable Long id){
+        Optional<Livro> livro =  service.buscarPorId(id);
+
+        if(livro.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(livro.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable long id) {
+        Optional<Livro> livro = service.buscarPorId(id);
+
+        /*como o metodo buscarporid retorna um optional (pode existir ou nao), o '.isEmpty'
+        é usado para verificar se nao ha valor dentro, ou seja, nesse caso, se nao encontrou o livro*/
+
+        if(livro.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletarPorId(id);
+        return  ResponseEntity.noContent().build();
+    }
+}
