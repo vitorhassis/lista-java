a54af738874991b51d1ba2178f75ce47282f604f
Vitor Hugo Assis <vitorassis997@gmail.com>
2026-03-23 17:09:17 -0300

    adiciona entidade fornecedor

---
+++ demo/src/main/java/Entity/Fornecedor.java
@@ -0,0 +1,51 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_fornecedor")
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeFantasia_fornecedor")
+    private String nomeFantasia;
+    @Column(name = "cnpj_fornecedor")
+    private String cnpj;
+    private  String contato;
+
+    public Fornecedor() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+}
