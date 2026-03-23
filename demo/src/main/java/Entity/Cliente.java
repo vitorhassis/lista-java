a3fd01a3c57d9f8e243f611e8661e1b12dd2ec0f
Vitor Hugo Assis <vitorassis997@gmail.com>
2026-03-23 16:58:16 -0300

    cria entidade Cliente

---
+++ demo/src/main/java/Entity/Cliente.java
@@ -0,0 +1,52 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome_cliente")
+    private String nome;
+    @Column(name = "email_cliente")
+    private String email;
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
