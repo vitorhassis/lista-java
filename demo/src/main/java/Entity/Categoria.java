9eec22d9997020def3e3e7a153fcda24c8b775d4
Vitor Hugo Assis <vitorassis997@gmail.com>
2026-03-23 00:31:02 -0300

    cria entidade categoria

---
+++ demo/src/main/java/Entity/Categoria.java
@@ -0,0 +1,40 @@
+package Entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "categoria_nome")
+    private String nome;
+    @Column(name = "categoria_descricao")
+    private String descricao;
+
+    public Categoria(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
